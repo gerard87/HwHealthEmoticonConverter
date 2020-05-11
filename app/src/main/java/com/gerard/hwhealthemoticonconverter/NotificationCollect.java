e8ab2f240a81cad358c7b5527e4fee69a8e769bf
gerard <gerar.f87@gmail.com>
2020-05-11 12:05:18 +0200

    Remove auto parser and title conversion

--- app/src/main/java/com/gerard/hwhealthemoticonconverter/NotificationCollect.java
+++ app/src/main/java/com/gerard/hwhealthemoticonconverter/NotificationCollect.java
@@ -2,8 +2,6 @@ package com.gerard.hwhealthemoticonconverter;
 
 import android.os.Bundle;
 
-import com.vdurmont.emoji.EmojiParser;
-
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedHelpers;
@@ -11,10 +9,8 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class NotificationCollect implements IXposedHookLoadPackage {
 
-    private boolean titleTextParser = true;
-    private boolean titleAlias = false;
+    private boolean titleTextParser = false;
     private boolean textTextParser = true;
-    private boolean textAlias = true;
 
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
@@ -26,18 +22,19 @@ public class NotificationCollect implements IXposedHookLoadPackage {
                 protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
                     Bundle bundle = (Bundle) param.args[0];
                     if (bundle != null) {
-                        String title = bundle.getString("data_extra_title");
-                        String text = bundle.getString("data_extra_text");
-
-                        if (title != null && (titleTextParser || titleAlias)) {
-                            if (titleTextParser) title = StringUtils.emoticonToTextParser(title);
-                            if (titleAlias) title = EmojiParser.parseToAliases(title);
-                            bundle.putString("data_extra_title", title);
+                        if (titleTextParser) {
+                            String title = bundle.getString("data_extra_title");
+                            if (title != null) {
+                                title = StringUtils.emoticonToTextParser(title);
+                                bundle.putString("data_extra_title", title);
+                            }
                         }
-                        if (text != null && (textTextParser || textAlias)) {
-                            if (textTextParser) text = StringUtils.emoticonToTextParser(text);
-                            if (textAlias) text = EmojiParser.parseToAliases(text);
-                            bundle.putString("data_extra_text", text);
+                        if (textTextParser) {
+                            String text = bundle.getString("data_extra_text");
+                            if (text != null) {
+                                text = StringUtils.emoticonToTextParser(text);
+                                bundle.putString("data_extra_text", text);
+                            }
                         }
                     }
                 }
